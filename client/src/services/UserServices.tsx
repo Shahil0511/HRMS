7073c18b1057e37d086c712a0da4a461cd7cf051
shahil kumar <109726576+Shahil0511@users.noreply.github.com>
2025-02-10 17:02:53 +0530

    Update UserServices.tsx

--- client/src/services/UserServices.tsx
+++ client/src/services/UserServices.tsx
@@ -8,7 +8,7 @@ export const fetchUserName = async () => {
             console.error("No token found, user might not be logged in.");
             throw new Error("No token found, user might not be logged in.");
         }
-        const response = await axios.get("http://localhost:8000/api/employees/user", {
+        const response = await axios.get("https://hrms-backend-7176.onrender.com/api/employees/user", {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
